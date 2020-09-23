e0fe108ee537a8075aed6b8bced3dd2e24a10158
Gibb Arko <gibbarko7@gmail.com>
2020-09-23 10:21:22 -0400

    Chapter 1 of intro to ruby

---
+++ the_basics.rb
@@ -0,0 +1,29 @@
+movies = {
+  finding_nemo: 2001,
+  incredibles: 2004,
+  toy_story: 1999,
+  mulan: 1996
+}
+
+movies.each {|x, y| puts y}
+
+puts "---------------"
+
+movies_arr = [ 2001, 2004, 1999, 1996 ]
+
+movies_arr.each { |x| puts x}
+
+puts "---------------"
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts "---------------"
+
+puts 6.23 ** 2
+puts 1345.234245 ** 2
+puts 0.345 ** 2
+
+puts "---------------"
